a896dc6f5b64b62ea7d52cd3f836bac9ad433fd6
Leonardo Franco <leofranco1305@gmail.com>
2020-06-07 13:26:57 -0300

    Added leaflet map usage v1

--- src/pages/CreateLocation/index.tsx
+++ src/pages/CreateLocation/index.tsx
@@ -1,123 +1,158 @@
-import React from 'react';
+import React, { useEffect, useState, ChangeEvent } from 'react';
 import { Link } from 'react-router-dom'
 import './styles.css';
 import logo from '../../assets/logo.svg'
 import { FiArrowLeft } from 'react-icons/fi'
 import { Map, TileLayer, Marker } from 'react-leaflet'
+import { LeafletMouseEvent } from 'leaflet'
+import api from '../../services/api'
+import axios from 'axios'
 
+interface Item {
+    id: number
+    title: string
+    image_url: string
+}
+
+interface UFResponse {
+    sigla: string
+}
 
 const CreateLocation = () => {
-  return (
-    <div id="page-create-point">
-        <header>
-            <img src={logo} alt="Ecoleta"/>
-            <Link to="/">
-                <FiArrowLeft />
-                Home
-            </Link>
-        </header>
+    const [items, setItems] = useState<Item[]>([])
+    const [ufs, setUfs] = useState<string[]>([])
+    const [selectedUf, setSelectedUf] = useState('0')
+    const [selectedPosition, setSelectedPosition] = useState<[number,number]>([0,0])
 
-        <form>
-            <h1>Location point register</h1>
+    useEffect(() => {
+        api.get('items').then(res => {
+            setItems(res.data.serializedItems)
+        })
+    }, [])
 
-            <fieldset>
-                <legend>
-                    <h2>Info</h2>
-                </legend>
+    useEffect(() => {
+        axios.get<UFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+            .then(res => {
+                const ufInitials = res.data.map(uf => uf.sigla)
+                setUfs(ufInitials)
+            })
+    }, [])
 
-                <div className="field">
-                    <label htmlFor="name">Entity name</label>
-                    <input type="text" name="name" id="name"/>                
-                </div>
+    useEffect(() => {
+        axios.get<UFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados/{UF}/municipios')
+            .then(res => {
+                const ufInitials = res.data.map(uf => uf.sigla)
+                setUfs(ufInitials)
+            })
+    }, [])
 
-                <div className="field-group">
+    function handleSelectedUF(event: ChangeEvent<HTMLSelectElement>) {
+        const uf = event.target.value
+        setSelectedUf(uf)
+    }
+    
+    function handleMapClick(event: LeafletMouseEvent) {
+        setSelectedPosition([
+            event.latlng.lat,
+            event.latlng.lng
+        ])
+    }
+
+    return (
+        <div id="page-create-point">
+            <header>
+                <img src={logo} alt="Ecoleta"/>
+                <Link to="/">
+                    <FiArrowLeft />
+                    Home
+                </Link>
+            </header>
+
+            <form>
+                <h1>Location point register</h1>
+
+                <fieldset>
+                    <legend>
+                        <h2>Info</h2>
+                    </legend>
+
                     <div className="field">
-                        <label htmlFor="email">E-mail</label>
-                        <input type="email" name="email" id="email"/>                
+                        <label htmlFor="name">Entity name</label>
+                        <input type="text" name="name" id="name"/>                
                     </div>
 
-                    <div className="field">
-                        <label htmlFor="wpp">Whatsapp</label>
-                        <input type="text" name="wpp" id="wpp"/>                
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="email">E-mail</label>
+                            <input type="email" name="email" id="email"/>                
+                        </div>
+
+                        <div className="field">
+                            <label htmlFor="wpp">Whatsapp</label>
+                            <input type="text" name="wpp" id="wpp"/>                
+                        </div>
                     </div>
-                </div>
-            </fieldset>
+                </fieldset>
 
-            <fieldset>
-                <legend>
-                    <h2>Address</h2>
-                    <span>Select the address in the map</span>
-                </legend>
+                <fieldset>
+                    <legend>
+                        <h2>Address</h2>
+                        <span>Select the address in the map</span>
+                    </legend>
 
-                <Map center={[-25.3843819, -49.2692895]} zoom={15} >
-                    <TileLayer
-                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    <Marker position={[-25.3843819, -49.2692895]}/>
-                </Map>
+                    <Map center={[-25.3843819, -49.2692895]} zoom={15} onClick={handleMapClick}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={selectedPosition}/>
+                    </Map>
 
-                <div className="field-group">
-                    <div className="field">
-                        <label htmlFor="uf">State</label>
-                        <select name="uf" id="uf">
-                            <option value="0">Select a state</option>
-                        </select>                    
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="uf">State</label>
+                            <select name="uf" id="uf" value={selectedUf} onChange= {handleSelectedUF}>
+                                <option value="0">Select a state</option>
+                                {ufs.map(uf => (
+                                    <option key={uf} value={uf}>{uf}</option>
+                                ))}
+                            </select>                    
+                        </div>
+
+                        <div className="field">
+                            <label htmlFor="city">City</label>
+                            <select name="city" id="city">
+                                <option value="0">Select a City</option>
+                            </select>                    
+                        </div>
                     </div>
 
-                    <div className="field">
-                        <label htmlFor="city">City</label>
-                        <select name="city" id="city">
-                            <option value="0">Select a City</option>
-                        </select>                    
-                    </div>
-                </div>
+                </fieldset>
 
-            </fieldset>
+                <fieldset>
+                    <legend>
+                        <h2>Collection Items</h2>
+                        <span>Select the items below</span>
+                    </legend>
 
-            <fieldset>
-                <legend>
-                    <h2>Collection Items</h2>
-                    <span>Select the items below</span>
-                </legend>
+                    <ul className="items-grid">
+                        {items.map(i => (
+                            <li key={i.id}>
+                                <img src={i.image_url} alt={i.title}/>
+                                <span>{i.title}</span>
+                            </li>
+                        ))}
+                    </ul>
 
-                <ul className="items-grid">
-                    <li>
-                        <img src="http://localhost:3333/uploads/batteries.svg" alt="goku"/>
-                        <span>Batteries</span>
-                    </li>
-                    <li className="selected">
-                        <img src="http://localhost:3333/uploads/batteries.svg" alt="goku"/>
-                        <span>Batteries</span>
-                    </li>
-                    <li>
-                        <img src="http://localhost:3333/uploads/batteries.svg" alt="goku"/>
-                        <span>Batteries</span>
-                    </li>
-                    <li>
-                        <img src="http://localhost:3333/uploads/batteries.svg" alt="goku"/>
-                        <span>Batteries</span>
-                    </li>
-                    <li>
-                        <img src="http://localhost:3333/uploads/batteries.svg" alt="goku"/>
-                        <span>Batteries</span>
-                    </li>
-                    <li>
-                        <img src="http://localhost:3333/uploads/batteries.svg" alt="goku"/>
-                        <span>Batteries</span>
-                    </li>
-                </ul>
-            </fieldset>
+                </fieldset>
 
-            <button type="submit">
-                Register location
-            </button>
+                <button type="submit">
+                    Register location
+                </button>
 
-        </form>
-    </div>
-
-
-  )
+            </form>
+        </div>
+    )
 }
 
 export default CreateLocation;
